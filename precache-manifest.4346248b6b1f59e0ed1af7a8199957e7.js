f8445f0d8d26f054bb2fb71e8ab0d8a143a5b9b6
unknown <frappyd@gmail.com>
2019-11-18 13:13:44 -0500

    Updates

---
+++ precache-manifest.4346248b6b1f59e0ed1af7a8199957e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b701a87ce3a6e84867d33e7138687c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b1914a5105b4854711e5",
+    "url": "/portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "16479721abdafdf23bc8",
+    "url": "/portfolio/static/js/2.55e1b789.chunk.js"
+  },
+  {
+    "revision": "b1914a5105b4854711e5",
+    "url": "/portfolio/static/js/main.4724a473.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
